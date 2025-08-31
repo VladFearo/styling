877ebd0ee719451f6bbcbcce2299eee5d67c0a69
Vlad Shevtsov <vladsh123@gmail.com>
2025-08-31 19:29:23 +0300

    Refactor AuthInputs component to use styled components for validation and remove unused CSS styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -15,16 +15,17 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({ invalid }) => (invalid ? "#ef4444" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({ invalid }) => (invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ invalid }) => (invalid ? "#374151" : "#374151")};
+  border: 1px solid;
+  border-color: ${({ invalid }) => (invalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -52,19 +53,19 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer className="controls">
-        <p>
-          <Label>Email</Label>
+        <div>
+          <Label invalid={emailNotValid}>Email</Label>
           <Input
+            invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
-        </p>
+        </div>
         <p>
-          <Label>Password</Label>
+          <Label invalid={emailNotValid}>Password</Label>
           <Input
+            invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
